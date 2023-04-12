629a864654ec493aad0f7b520e45177235fa48f1
rizla <ryanpauldutton@gmail.com>
2023-04-12 21:36:24 +0100

    added ero section and navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,6 +15,18 @@ const Navbar = () => (
     <div className="absolute w-[50%] inset-0
     gradient-01"
     />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="font-extrabold leading-[30px] text-[24px] text-white">27 Studio</h2>
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain" />
+    </div>
   </m.nav>
 );
 
