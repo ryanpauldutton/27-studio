c7d07daee8e3113238690676e0f45606f4f12448
rizla <ryanpauldutton@gmail.com>
2023-04-13 22:11:47 +0100

    made good progress upto the explore section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,42 @@
 'use client';
 
+import { motion as m } from 'framer-motion';
+import { TypingText } from '../components'
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02" />
+    <m.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title=" about me"
+      textStyles="text-center" />
+
+      <m.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Metaverse </span>
+        is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the 
+        <span className="font-extrabold text-white"> madness of the metaverse of today, using only VR </span>
+        devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down
+      </m.p>
+
+      <m.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-28[px] object-contain mt-28px"
+      />
+      About section
+    </m.div>
   </section>
 );
 
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,7 +1,30 @@
 'use client';
 
+import { useState } from 'react';
+import { motion as m } from 'framer-motion';
+import { ExploreCard, TitleText, TypingText } from '../components'
+
+import styles from '../styles';
+import { fadeIn, slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { exploreWorlds } from '../constants'
+
 const Explore = () => (
-  <section>
+  <section className={`${styles.paddings}`}
+  id="explore"
+  >
+    <m.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="My Work"
+      textStyles="text-center" />
+      <TitleText title={<>Choose the world you want
+      <br className="md:block hidden" /> to Explore </>}
+      textStyles="text-center" />
+      </m.div>
     Explore section
   </section>
 );
