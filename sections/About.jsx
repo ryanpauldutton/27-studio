6bc89959a27373b2d15d6a6078f4c49e0d37b0db
rizla <ryanpauldutton@gmail.com>
2023-04-15 08:28:15 +0100

    added explore accordion section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion as m } from 'framer-motion';
-import { TypingText } from '../components'
+import { TypingText } from '../components';
 
 import styles from '../styles';
 import { fadeIn, staggerContainer } from '../utils/motion';
@@ -13,20 +13,21 @@ const About = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: false, amount: 0.25}}
+      viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title=" about me"
-      textStyles="text-center" />
+        textStyles="text-center"
+      />
 
       <m.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
         <span className="font-extrabold text-white">Metaverse </span>
-        is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the 
-        <span className="font-extrabold text-white"> madness of the metaverse of today, using only VR </span>
-        devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down
+        is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the
+        <span className="font-extrabold text-white"> madness of the meta verse of today, using only VR </span>
+        devices you can easily explore the meta verse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down
       </m.p>
 
       <m.img
@@ -35,7 +36,6 @@ const About = () => (
         alt="arrow down"
         className="w-[18px] h-28[px] object-contain mt-28px"
       />
-      About section
     </m.div>
   </section>
 );
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -2,31 +2,50 @@
 
 import { useState } from 'react';
 import { motion as m } from 'framer-motion';
-import { ExploreCard, TitleText, TypingText } from '../components'
+import { ExploreCard, TitleText, TypingText } from '../components';
 
 import styles from '../styles';
-import { fadeIn, slideIn, staggerContainer, textVariant } from '../utils/motion';
-import { exploreWorlds } from '../constants'
+import { staggerContainer } from '../utils/motion';
+import { exploreWorlds } from '../constants';
 
-const Explore = () => (
-  <section className={`${styles.paddings}`}
-  id="explore"
-  >
-    <m.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{once: false, amount: 0.25}}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
+const Explore = () => {
+  const [active, setactive] = useState('world-2');
+
+  return (
+    <section className={`${styles.paddings}`}
+      id="explore"
     >
-      <TypingText title="My Work"
-      textStyles="text-center" />
-      <TitleText title={<>Choose the world you want
-      <br className="md:block hidden" /> to Explore </>}
-      textStyles="text-center" />
+      <m.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="My Work"
+          textStyles="text-center"
+        />
+        <TitleText title={(
+          <>Choose the world you want
+            <br className="md:block hidden" /> to Explore
+          </>
+        )}
+          textStyles="text-center"
+        />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              handleClick={setactive}
+            />
+          ))}
+        </div>
       </m.div>
-    Explore section
-  </section>
-);
+    </section>
+  );
+};
 
 export default Explore;
