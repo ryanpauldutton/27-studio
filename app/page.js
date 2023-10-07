c2132f13d097976abb8418c73f78b71dd0806ed5
rizla <ryanpauldutton@gmail.com>
2023-10-07 12:28:15 +0100

    trial

--- app/page.js
+++ app/page.js
@@ -7,32 +7,34 @@ import ThreeDType from '../components/ThreeDType'
 import ScrollType from '../components/ScrollType'
 import { useState } from 'react';
 import Projects from '../components/Projects';
-import Description from 'components/Description';
-import SlidingImages from '/components/SlidingImages';
+import Description from '..components/Description';
+import SlidingImages from '../components/SlidingImages';
 import Contact from '../components/Contact';
 
 
 
-export default function Home() {
-  
 
-  const [modal, setModal] = useState({active: false, index: 0})  
+
+export default function Home() {
+
+
+  const [modal, setModal] = useState({ active: false, index: 0 })
   return (
 
     <main className="flex flex-col relative bg-slate-300 overflow-hidden">
-       
-<div className='absolute w-full h-screen z-30 flex flex-col-reverse p-2 '>
+
+      <div className='absolute w-full h-screen z-30 flex flex-col-reverse p-2 '>
         <ScrollType />
-        </div>
+      </div>
 
       <ThreeDType />
 
       <Description />
       <Projects />
       <SlidingImages />
-<Contact />
+      <Contact />
     </main>
-    
-   
+
+
   )
 }
