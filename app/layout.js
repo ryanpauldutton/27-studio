27806a135c7e41e122d75e565953292cf3730bcb
rizla <ryanpauldutton@gmail.com>
2023-10-08 11:48:10 +0100

    trial2

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,8 @@ export default function RootLayout({ children }) {
         
         {/* </SmoothScroll> */}
         <CustomCursor/>
-        
+        <SlidingImages />
+        <Contact />
       </body>
     </html>
   )
--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,3 +1,5 @@
+'use client'
+
 import styles from './style.module.scss';
 import Image from 'next/image';
 import Rounded from '../../common/RoundedButton';
