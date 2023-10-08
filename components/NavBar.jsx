ec9ce0b8cb7b97a02260a6273699fe7229134391
rizla <ryanpauldutton@gmail.com>
2023-10-08 16:22:47 +0100

    yo

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -7,7 +7,7 @@ import { TestMove } from "./CustomAni";
 const Navbar = () => (
     <nav className="fixed z-50 bg-blend-difference mix-blend-difference w-full flex justify-between p-2 text-white">
 
-        <Link href='/'><h2 className="font-bold leading-[30px] text-[20px]">27-STUDIO</h2></Link>
+        <Link href='/'><h2 className="font-bold leading-[30px] text-[20px]">27 STUDIO</h2></Link>
         <ul className="flex gap-4">
            
         <a href ="/public/images/ryandutton-cv.pdf" download ><li> <TestMove className="px-2" text="CV" /></li></a>
